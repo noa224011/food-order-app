de14b78a78283857d0e5762b1ba9258b73f5f2d0
Noa Shay <noa224011224011@gmail.com>
2021-10-27 15:53:15 +0300

    insert cartButton component into Header

--- client/src/components/Layout/Header/Header.js
+++ client/src/components/Layout/Header/Header.js
@@ -1,12 +1,14 @@
 import classes from "./Header.module.css";
 import { Fragment } from "react";
 import mealsImage from "../../../assets/meals.jpg";
+import HeaderCartButton from "../HeaderCartButton/HeaderCartButton";
 
 function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Food Order App</h1>
+        <HeaderCartButton></HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
--- client/src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ client/src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -3,12 +3,12 @@ import CartIcon from "../../Cart/CartIcon";
 
 function HeaderCartButton(props) {
   return (
-    <button className={btnClasses}>
+    <button className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{numberOfCartItems}</span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 }
