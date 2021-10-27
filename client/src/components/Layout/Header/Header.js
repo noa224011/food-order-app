df541f4ac304af4ad56524c199bbf473f04b3051
Noa Shay <noa224011224011@gmail.com>
2021-10-27 15:43:00 +0300

    Header component

---
+++ client/src/components/Layout/Header/Header.js
@@ -0,0 +1,19 @@
+import classes from "./Header.module.css";
+import { Fragment } from "react";
+import mealsImage from "../../../assets/meals.jpg";
+import HeaderCartButton from "../HeaderCartButton/HeaderCartButton";
+
+function Header(props) {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Food Order App</h1>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
+}
+
+export default Header;
