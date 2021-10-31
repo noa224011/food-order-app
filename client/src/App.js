a0cf6da0763b632d812da1b82da7a790f3b9208f
Noa Shay <noa224011224011@gmail.com>
2021-10-31 11:55:04 +0200

    managing state of cart in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Cart from "./components/Cart/Cart/Cart";
 import Header from "./components/Layout/Header/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
-      <Header />
-      <Cart />
+      <Header onShowCart={showCartHandler} />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
       <main>
         <Meals />
       </main>
