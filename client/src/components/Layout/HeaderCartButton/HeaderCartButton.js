837228495905e109587e627dfe080a1a3dc187cf
Noa Shay <noa224011224011@gmail.com>
2021-10-31 12:03:27 +0200

    calculate number of cart items and display

--- client/src/components/Layout/HeaderCartButton/HeaderCartButton.js
+++ client/src/components/Layout/HeaderCartButton/HeaderCartButton.js
@@ -1,14 +1,22 @@
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../../Cart/CartIcon";
+import CartContext from "../../../store/cart-context";
+import { useContext } from "react";
 
 function HeaderCartButton(props) {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((acc, cur) => {
+    return acc + cur.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 }
