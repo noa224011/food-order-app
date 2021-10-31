b6f63f4f82c9306726e3ad4dd2a18529d0d161f3
Noa Shay <noa224011224011@gmail.com>
2021-10-31 12:31:33 +0200

    validation when adding items to cart

--- client/src/components/Meals/MealItem/MealItemForm/MealItemForm.js
+++ client/src/components/Meals/MealItem/MealItemForm/MealItemForm.js
@@ -1,10 +1,33 @@
 import classes from "./MealItemForm.module.css";
 import Input from "../../../UI/Input/Input";
+import { useRef, useState } from "react";
 
 function MealItemForm(props) {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmount < 1 ||
+      enteredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +39,7 @@ function MealItemForm(props) {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid number [1-5]</p>}
     </form>
   );
 }
