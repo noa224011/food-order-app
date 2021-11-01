82046a1bec13b7dcf766e78a89cf3a6bd25abe18
Noa Shay <noa224011224011@gmail.com>
2021-11-01 18:47:14 +0200

    spinner component

---
+++ client/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import "./Spinner.css";
+
+function Spinner() {
+  return (
+    <div class="loading-container">
+      <div class="spinner"></div>
+      <div class="spinner-center"></div>
+      <div class="loading-text">Loading...</div>
+    </div>
+  );
+}
+
+export default Spinner;
